437aef360af146324dc5dfcfcfb0638198c48594
Marc Bernstein <marcbernstein98@gmail.com>
2022-11-05 18:14:38 -0400

    have nginx use a dynamic port

--- frontend/container/scripts/set-env.sh
+++ frontend/container/scripts/set-env.sh
@@ -4,7 +4,11 @@
 set -e
 
 if [ -n "$REACT_APP_MATCH_DISPLAY_URL" ]; then
-sed -i -e "s|REPLACE_MATCH_DISPLAY_URL|$REACT_APP_MATCH_DISPLAY_URL|g" /app/container/scripts/sample.env 
+sed -i -e "s|REPLACE_MATCH_DISPLAY_URL|$REACT_APP_MATCH_DISPLAY_URL|g" /app/container/templates/sample.env.template 
+cat /app/container/templates/sample.env.template  > /app/.env 
 fi
 
-cat /app/container/scripts/sample.env > /app/.env 
+# if [ -n "$PORT" ]; then
+# echo $PORT
+# sed -i -e "s|PORT|$PORT|g" /app/container/templates/default.conf.template
+# fi
