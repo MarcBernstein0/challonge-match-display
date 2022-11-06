82f99359ef6acee2659f632e8fe756efa9faa182
Marc Bernstein <marcbernstein98@gmail.com>
2022-11-06 16:47:18 -0500

    see if react app is being set

--- frontend/container/scripts/set-env.sh
+++ frontend/container/scripts/set-env.sh
@@ -3,7 +3,10 @@
 # Create env file
 set -e
 
+echo Setting Env File
+
 if [ -n "$REACT_APP_MATCH_DISPLAY_URL" ]; then
+echo $REACT_APP_MATCH_DISPLAY_URL
 sed -i -e "s|REPLACE_MATCH_DISPLAY_URL|$REACT_APP_MATCH_DISPLAY_URL|g" /app/container/templates/sample.env.template 
 cat /app/container/templates/sample.env.template  > /app/.env 
 fi
